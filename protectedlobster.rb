3852a8ec207ce1e543217dc95d5cfecc9cb24550
Casiano Rodriguez Leon <casiano.rodriguez.leon@gmail.com>
2013-10-14 10:41:37 +0100

    Rakefile with protected targets

--- protectedlobster.rb
+++ protectedlobster.rb
@@ -4,11 +4,10 @@ require './lobster'
 lobster = Rack::Lobster.new
 
 protected_lobster = Rack::Auth::Basic.new(lobster) do |username, password|
-  'secret' == password
+  'secreto' == password
 end
 
 protected_lobster.realm = 'Lobster 2.0'
-
 pretty_protected_lobster = Rack::ShowStatus.new(Rack::ShowExceptions.new(protected_lobster))
 
-Rack::Server.start :app => pretty_protected_lobster, :Port => 9493
+Rack::Server.start :app => pretty_protected_lobster, :Port => 9292
